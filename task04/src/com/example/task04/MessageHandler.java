1e4e0c854efac8bd1d1e313c26ab8e7441af3a9d
ShmelZhopnyi <craftritch23@gmail.com>
2026-02-27 11:19:33 +0500

    Мессадж

--- task04/src/com/example/task04/MessageHandler.java
+++ task04/src/com/example/task04/MessageHandler.java
@@ -1,10 +1,6 @@
 package com.example.task04;
 
-import java.io.IOException;
-
 public interface MessageHandler {
-
-    void publish(String formattedMessage) throws IOException;
-    void close() throws IOException;
-
+    //выводит или записывает сообщение
+    void log(String message);
 }
